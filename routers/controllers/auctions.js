6d7fbba39998d31986dc0dc843d0c9319f726fd7
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-12-21 10:50:47 +0300

    home auctions route & controller

--- routers/controllers/auctions.js
+++ routers/controllers/auctions.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const getAuctions = (req, res) => {
   auctionsModel
-    .find({})
+    .find({ status: process.env.APPROVED_STATUS, sold: false })
     .then((result) => {
       if (result.length > 0) res.status(200).json(result);
       else res.status(404).json({ message: "there is no auctions yet!!" });
@@ -20,13 +20,29 @@ const getAuction = (req, res) => {
   const { id } = req.params;
 
   auctionsModel
-    .findOne({ _id: id })
+    .findOne({ _id: id, status: process.env.APPROVED_STATUS, sold: false })
     .then((result) => {
       if (result) res.status(200).json(result);
       else
         res
           .status(404)
           .json({ message: `there is no auction with the ID: ${id}` });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+const getHomeAuctions = (req, res) => {
+  auctionsModel
+    .find({ status: process.env.APPROVED_STATUS, sold: false })
+    .sort({ endDateTime: 1 })
+    .then((result) => {
+      if (result.length > 0)
+        res
+          .status(200)
+          .json({ lastMin: result.slice(-10), new: result.slice(0, 10) });
+      else res.status(404).json({ message: "there is no auctions yet!!" });
     })
     .catch((err) => {
       res.status(400).json(err);
@@ -68,4 +84,4 @@ const createAuction = (req, res) => {
     });
 };
 
-module.exports = { getAuctions, getAuction, createAuction };
+module.exports = { getAuctions, getAuction, getHomeAuctions, createAuction };
