5dcf920e6c124880bccdace47ea90b8cdf218ae2
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2022-01-03 10:04:07 +0300

    payment routers

--- routers/controllers/invoices.js
+++ routers/controllers/invoices.js
@@ -1,4 +1,6 @@
 const invoicesModel = require("./../../db/models/invoices");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const getInvoices = (req, res) => {
   invoicesModel
@@ -13,4 +15,48 @@ const getInvoices = (req, res) => {
     });
 };
 
-module.exports = { getInvoices };
+const updatePayment = (req, res) => {
+  const { id } = req.params;
+
+  invoicesModel
+    .findOneAndUpdate(
+      { _id: id, buyer: req.token.id },
+      { paid: true, status: process.env.APPROVED_STATUS },
+      { new: true }
+    )
+    .then((result) => {
+      if (result) res.status(200).json(result);
+      else
+        res
+          .status(404)
+          .json({ message: `there is no invoice with the ID ${id}` });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+const payment = async (req, res) => {
+  const { items } = req.body;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "sar",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+};
+
+module.exports = { getInvoices, updatePayment, payment };
--- routers/routes/invoices.js
+++ routers/routes/invoices.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
-const { getInvoices } = require("../controllers/invoices");
+const { getInvoices, updatePayment, payment } = require("../controllers/invoices");
 const authentication = require("../middlewares/authentication");
 
 const invoicesRouter = express.Router();
 
 invoicesRouter.get("/invoices", authentication, getInvoices);
+invoicesRouter.put("/invoices/:id", authentication, updatePayment);
+invoicesRouter.post("/create-payment-intent", payment);
 
 module.exports = invoicesRouter;
